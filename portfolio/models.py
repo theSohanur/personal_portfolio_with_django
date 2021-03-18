da17e10ddf1f94ed535d4d0f51da3d738f9fd297
Sohanur Rahman <sohanur064@gmail.com>
2021-03-18 22:17:06 +0600

    makemigrations and set model to admin

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 # Create your models here.
-class Portfolio(models.Model):
+class Project(models.Model):
     title = models.CharField(max_length=100)
     desc = models.CharField(max_length=250)
     image = models.ImageField(upload_to='portfolio/images/')
