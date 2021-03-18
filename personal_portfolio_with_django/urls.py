c8eb34b36269157cbcb77a108676b44922f54d78
Sohanur Rahman <sohanur064@gmail.com>
2021-03-19 00:15:03 +0600

    add templete and using database objects

--- personal_portfolio_with_django/urls.py
+++ personal_portfolio_with_django/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from portfolio.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
