4a8bdde14631e8bee7e6d21e4064c87158f41525
Sohanur Rahman <sohanur064@gmail.com>
2021-03-19 07:22:19 +0600

    add blog urls and Create blog models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    desc = models.TextField()
+    date = models.DateField()
